80a8ddbf856d1e6f6e607aaab75c69fc0a8505cd
Reyad Hasan <reyad.hasan@pm.me>
2025-07-16 21:46:46 +0600

    Create App.js

---
+++ react-reyad/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>Welcome to My React App</h1>
+      <p>Created by <strong style={{ color: 'blue' }}>Reyad</strong></p>
+    </div>
+  );
+}
+
+export default App;
